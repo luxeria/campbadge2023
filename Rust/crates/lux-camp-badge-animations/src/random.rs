5c373fd9a88583d95f4bd9e4316d22be23d6e960
xermicus <cyrill@parity.io>
2023-07-15 13:39:24 +0200

    simplify random

--- Rust/crates/lux-camp-badge-animations/src/random.rs
+++ Rust/crates/lux-camp-badge-animations/src/random.rs
@@ -3,11 +3,9 @@ use rand::{rngs::SmallRng, Rng, SeedableRng};
 use smart_leds_trait::{SmartLedsWrite, RGB8};
 use std::time::Duration;
 
-/// Every 250ms, each pixel has a 30% chance of getting colored randomly.
-pub struct Random {
-    last_tick: Duration,
-    rng: SmallRng,
-}
+/// Each pixel has a 30% chance of getting colored randomly on each frame.
+/// The frame rate is choosen at random between 100ms an 1s.
+pub struct Random(SmallRng);
 
 impl Random {
     pub fn boxed<Matrix, Driver>(seed: u64) -> Box<dyn Animation<Matrix> + Send>
@@ -15,10 +13,7 @@ impl Random {
         Matrix: LedMatrix<Driver = Driver>,
         Driver: SmartLedsWrite<Color = RGB8>,
     {
-        Box::new(Self {
-            last_tick: Duration::ZERO,
-            rng: SmallRng::seed_from_u64(seed),
-        })
+        Box::new(Self(SmallRng::seed_from_u64(seed)))
     }
 }
 
@@ -27,16 +22,14 @@ where
     B: SmartLedsWrite<Color = RGB8>,
 {
     fn init(&mut self, _matrix: &mut C) -> Option<Duration> {
-        Some(Duration::from_millis(self.rng.gen_range(100..1000)))
+        Some(Duration::from_millis(self.0.gen_range(100..1000)))
     }
 
-    fn update(&mut self, tick: Duration, matrix: &mut C) {
-        self.last_tick = tick;
-
+    fn update(&mut self, _tick: Duration, matrix: &mut C) {
         let mut buf = Vec::with_capacity(<C as LedMatrix>::AREA);
         for _ in 0..<C as LedMatrix>::AREA {
-            buf.push(if self.rng.gen_bool(0.3) {
-                RGB8::new(self.rng.gen(), self.rng.gen(), self.rng.gen())
+            buf.push(if self.0.gen_bool(0.3) {
+                RGB8::new(self.0.gen(), self.0.gen(), self.0.gen())
             } else {
                 RGB8::new(0, 0, 0)
             })
