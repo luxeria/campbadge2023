d1b94518263556636a0ffe5104595547961440ec
Raphael Theiler <rtheiler@esheep.ch>
2023-07-16 14:40:08 +0200

    removed external perlin noise crates and started to write a simplified one for our needs

--- Rust/crates/lux-camp-badge-animations/src/noise_animation.rs
+++ Rust/crates/lux-camp-badge-animations/src/noise_animation.rs
@@ -1,25 +1,45 @@
 use lux_camp_badge::led::{Animation, LedMatrix};
-extern crate fuss;
-use fuss::Simplex;
+
+use rand::{rngs::SmallRng, Rng, RngCore, SeedableRng};
 use smart_leds_trait::{SmartLedsWrite, RGB8};
 use std::time::Duration;
+use smart_leds::hsv::{Hsv, hsv2rgb};
 
+struct PerlinNoiseLight {
+    seed: Vec<Vec<Vec<u8>>>,
+    frame: usize,
+}
 
-pub struct PerlinAnimation(Simplex);
+impl PerlinNoiseLight {
+    fn new(width: usize, height: usize) -> PerlinNoiseLight {
+        let mut rnd = SmallRng::seed_from_u64(1);
+        let mut seed = vec![vec![vec![127; 100]; height]; width];
+        for x in 0..width{
+            for y in 0..height{
+                for t in 0..100{
+                    seed[x][y][t] = rnd.gen();
+                }
+            }
+        }
+        PerlinNoiseLight { seed , frame: 0}
+    }
+}
+
+pub struct PerlinAnimation(PerlinNoiseLight);
 
 impl PerlinAnimation {
     pub fn build<Matrix, Driver>() -> Box<dyn Animation<Matrix> + Send>
-        where
-            Matrix: LedMatrix<Driver = Driver>,
-            Driver: SmartLedsWrite<Color = RGB8>,
+    where
+        Matrix: LedMatrix<Driver = Driver>,
+        Driver: SmartLedsWrite<Color = RGB8>,
     {
-        Box::new(Self(Simplex::new()))
+        Box::new(Self(PerlinNoiseLight::new(Matrix::X, Matrix::Y)))
     }
 }
 
 impl<B, C: LedMatrix<Driver = B>> Animation<C> for PerlinAnimation
-    where
-        B: SmartLedsWrite<Color = RGB8>,
+where
+    B: SmartLedsWrite<Color = RGB8>,
 {
     fn init(&mut self, _matrix: &mut C) -> Option<Duration> {
         Some(Duration::from_millis(250))
@@ -27,13 +47,12 @@ impl<B, C: LedMatrix<Driver = B>> Animation<C> for PerlinAnimation
 
     fn update(&mut self, _tick: Duration, matrix: &mut C) {
         let mut buf = Vec::with_capacity(<C as LedMatrix>::AREA);
-        /*for _ in 0..<C as LedMatrix>::AREA {
-            buf.push(if self.0.gen_bool(0.3) {
-                RGB8::new(self.0.gen(), self.0.gen(), self.0.gen())
-            } else {
-                RGB8::new(0, 0, 0)
-            })
-        }*/
+        for _ in 0..<C as LedMatrix>::AREA {
+            buf.push( hsv2rgb(Hsv{hue: self.0.seed[1][1][self.0.frame],sat:255,val:25}));
+        }
+        let frame = self.0.frame;
+        self.0.frame = (frame +1) % 100;
+
         matrix.set_buf(&mut buf);
     }
 }
